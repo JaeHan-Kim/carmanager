eb269cef9093722e634db1f802817f97a0e35e5c
JaeHan-Kim <hgal8877@gmail.com>
2016-01-08 11:47:44 +0900

    설정 파일, JAVA 소스 코드 추가

---
+++ carmanager/src/main/java/cms/controller/GarageController.java
@@ -0,0 +1,76 @@
+package cms.controller;
+
+import java.util.HashMap;
+import java.util.List;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpSession;
+
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import cms.domain.Garage;
+import cms.domain.Member;
+import cms.domain.Refuel;
+import cms.service.GarageService;
+import cms.service.RefuelService;
+
+@Controller
+@RequestMapping("/main/*")
+public class GarageController {
+  public static final Logger log = Logger.getLogger(GarageController.class);
+  
+  @Autowired GarageService garageService;
+  @Autowired RefuelService refuelService;
+  @Autowired ServletContext servletContext;
+  
+  @RequestMapping("list")
+  public Object carList(
+  		HttpSession session) throws Exception {
+
+  	System.out.println("들어간다 ");
+  	//Member member = (Member)session.getAttribute("loginUser");
+  	//log.debug(member.toString());
+		int no = 1;
+    List<Garage> garages = garageService.listAll(no);
+    List<Refuel> refuels = refuelService.listAll(no);
+    List<Refuel> costavg = refuelService.costAvg();
+    //for (Garage g : garages) {
+    //  log.debug(g.toString());
+    //}
+    //for (Refuel r : refuels) {
+    //  log.debug(r.toString());
+    //}
+    //for (Refuel r : costavg) {
+    // log.debug(r.toString());
+    //}
+    
+    HashMap<String, Object> resultMap = new HashMap<>();
+    resultMap.put("status", "success");
+    resultMap.put("data", garages);
+    resultMap.put("refuelList", refuels);
+    resultMap.put("memCostAvg", costavg);
+    return resultMap;  
+
+  }
+  @RequestMapping(value="select", method=RequestMethod.GET)
+  public Object selList(int no,
+  		HttpSession session) throws Exception {
+  	log.debug("현재 차량은 " + no);
+  	
+    List<Refuel> refuels = refuelService.listAll(no);
+    List<Refuel> costavg = refuelService.costAvg();
+    for (Refuel r : refuels) {
+      log.debug(r.toString());
+    }
+  	HashMap<String, Object> resultMap = new HashMap<>();
+  	resultMap.put("status", "success");
+    resultMap.put("refuelList", refuels);
+    resultMap.put("memCostAvg", costavg);
+    return resultMap; 
+  }
+  
+}
