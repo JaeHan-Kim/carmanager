f0ba5f39b7c0347cead869e806155220fb2b419b
taewoong-yun <inpotin03@gmail.com>
2016-01-11 20:51:56 +0900

    오늘 작업 마지막 커밋

--- carmanager/src/main/java/cms/controller/GarageController.java
+++ carmanager/src/main/java/cms/controller/GarageController.java
@@ -18,9 +18,9 @@ import cms.service.GarageService;
 import cms.service.RefuelService;
 
 @Controller
-// @RequestMapping("/main/*")
+@RequestMapping("/garage/*")
 public class GarageController {
-  /*public static final Logger log = Logger.getLogger(GarageController.class);
+  public static final Logger log = Logger.getLogger(GarageController.class);
   
   @Autowired GarageService garageService;
   @Autowired RefuelService refuelService;
@@ -28,41 +28,15 @@ public class GarageController {
   
   @RequestMapping("list")
   public Object carList(
-  		HttpSession session) throws Exception {
+  		HttpSession session, int no) throws Exception {
 
-		int no = 1;
     List<Garage> garages = garageService.listAll(no);
-    List<Refuel> refuels = refuelService.listAll(no);
-    List<Refuel> costAvg = refuelService.costAvg();
-    List<Refuel> myCostAvg = refuelService.myCostAvg(no);
     
     HashMap<String, Object> resultMap = new HashMap<>();
     resultMap.put("status", "success");
     resultMap.put("data", garages);
-    resultMap.put("refuelList", refuels);
-    resultMap.put("memCostAvg", costAvg);
-    resultMap.put("myCostAvg", myCostAvg);
     return resultMap;  
 
   }
-  
-  @RequestMapping(value="select", method=RequestMethod.GET)
-  public Object selList(int no,
-  		HttpSession session) throws Exception {
-  	//log.debug("현재 차량은 " + no + "입니다.");
-  	
-  	List<Garage> garages = garageService.listAll(no);
-    List<Refuel> refuels = refuelService.listAll(no);
-    List<Refuel> costAvg = refuelService.costAvg();
-    List<Refuel> myCostAvg = refuelService.myCostAvg(no);
-    
-    HashMap<String, Object> resultMap = new HashMap<>();
-    resultMap.put("status", "success");
-    resultMap.put("data", garages);
-    resultMap.put("refuelList", refuels);
-    resultMap.put("memCostAvg", costAvg);
-    resultMap.put("myCostAvg", myCostAvg);
-    return resultMap; 
-  }*/
   
 }
