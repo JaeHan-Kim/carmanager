bfce596d7f234395b34f5cd9eab5381b93789b29
정다인 <doem456@hanmail.net>
2016-01-11 14:19:18 +0900

    session 정보 관련 기능 추가 및 버그 수정

--- carmanager/src/main/java/cms/controller/AuthController.java
+++ carmanager/src/main/java/cms/controller/AuthController.java
@@ -1,78 +1,50 @@
-/*package cms.controller;
+package cms.controller;
 
 import java.util.HashMap;
 
-import javax.servlet.http.Cookie;
+import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import cms.dao.MemberDao;
+import cms.domain.AjaxResult;
 import cms.domain.Member;
 
-@Controller
-@RequestMapping("/auth/*")
-public class AuthController {
+@Controller("ajax.AuthController")
+@RequestMapping("/auth/ajax/*")
+public class AuthController {   
+  static public Logger log = Logger.getLogger(AuthController.class);
+
   @Autowired MemberDao memberDao;
-  
-  @RequestMapping(value="login", method=RequestMethod.GET)
-  public String loginform() {
-    return "login";
-  }
+  @Autowired ServletContext servletContext;
   
   @RequestMapping(value="login", method=RequestMethod.POST)
-  public String login(
-      @RequestParam(defaultValue="0")int memberNo,
+  public AjaxResult login(
       String email,
-      @RequestParam(defaultValue="a")String nickName,
       String password,
-      @RequestParam(defaultValue="a") String memberPhoto,
-      @RequestParam(defaultValue="0")int grade,
-      String saveEmail,
       HttpServletResponse response,
       HttpSession session) {
-    
-    Cookie emailCookie = null;
-    if (saveEmail != null) {
-      emailCookie = new Cookie("email", email);
-      emailCookie.setMaxAge(60 * 60 * 24 * 7);
-    } else {
-      emailCookie = new Cookie("email", "");
-      emailCookie.setMaxAge(0);
-    }
-    response.addCookie(emailCookie);
-    
+	  
     HashMap<String, Object> paramMap = new HashMap<>();
-    paramMap.put("memberNo", memberNo);
     paramMap.put("email", email);
-    paramMap.put("nickName", nickName);
     paramMap.put("password", password);
-    paramMap.put("memberPhoto", memberPhoto);
-    paramMap.put("grade", grade);
-    
+        
     Member member = memberDao.selectOneByEmailPassword(paramMap);
-    
-    System.out.println(member);
     
     if (member == null) {
       session.invalidate();
-      return "loginFail";
+      return new AjaxResult("failure", null);
     }
     
-    session.setAttribute("loginUser", member);
+//    session.setAttribute("loginUser", member);
     
-    return "redirect:../main/main.html";
+    return new AjaxResult("success", member);
   }
   
-  @RequestMapping("logout")
-  public String logout(HttpSession session) {
-    session.invalidate();
-    return "redirect:login.do";
-  }
 }
-*/
