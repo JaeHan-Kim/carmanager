fecfe436f830e1c40faf717d0e60cff6998be109
taewoong-yun <inpotin03@gmail.com>
2016-01-12 12:06:26 +0900

    차량 리스트 불러오기 성공

--- carmanager/src/main/java/cms/controller/GarageController.java
+++ carmanager/src/main/java/cms/controller/GarageController.java
@@ -9,9 +9,9 @@ import javax.servlet.http.HttpSession;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import cms.domain.Garage;
-import cms.domain.Refuel;
 import cms.service.GarageService;
 import cms.service.RefuelService;
 
