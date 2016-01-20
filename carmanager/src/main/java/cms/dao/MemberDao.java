a460fa4a093eb8e0186f661a4c50d5bd93e013f8
Song <ho4319@naver.com>
2016-01-20 20:49:49 +0900

    mypage.html 비밀번호 유효성검증, 비밀번호 일치여부 검사기능 추가, 비밀번호 변경기능 추가, 세부 폼 수정

--- carmanager/src/main/java/cms/dao/MemberDao.java
+++ carmanager/src/main/java/cms/dao/MemberDao.java
@@ -7,14 +7,16 @@ import cms.domain.Member;
 public interface MemberDao {
   
   int insert(Member member);
+
+  int update(Member member);
   
+  Member selectOne(int no);
+
   Member selectOneByEmailPassword(Map<String,Object> paramMap);
   
   int checkEmail(Member member);
   
   int checkNickname(Member member);
   
-  Member selectOne(int no);
   
-  int update(Member member);
 }
