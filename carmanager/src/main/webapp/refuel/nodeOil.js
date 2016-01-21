c34ddf6305b2f94ba78aa9d88fbc1ee3354085b9
taewoong-yun <inpotin03@gmail.com>
2016-01-21 20:14:04 +0900

    연료 정보를 불러오기 위한 sql 수정

--- carmanager/src/main/webapp/refuel/nodeOil.js
+++ carmanager/src/main/webapp/refuel/nodeOil.js
@@ -1,5 +1,10 @@
-var express = require('express');
-var app = express();
+// 유가 정보 불러오기 위한 라이브러리 경로 
+// cd /Users/yuntaeung/git/carmanager/carmanager
+// 설치 : npm install unirest 
+// node.js 실행 위한 js 파일 주소 경로
+// cd /Users/yuntaeung/git/carmanager/carmanager/src/main/webapp/nodeServer
+// url : http://localhost:8989/oil
+
 var http = require('http');
 var url = require('url'); 
 var unirest = require('unirest');
@@ -12,7 +17,7 @@ var httpServer = http.createServer(function(request, response){
     Request.header("x-waple-authorization", "Mzc0NC0xNDUzMTY3OTQzMjQwLTljMDZhN2JkLWQ0NjUtNDE2Yy04NmE3LWJkZDQ2NTQxNmM4Zg==")
     .end(function (result) {
       
-      response.write(result.body);
+      response.end(result.body);
       console.log(result.body);
     });
     return;
